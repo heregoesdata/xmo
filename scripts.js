9678c86c5e0b970bc01638fbbd3f2e0455eea3c0
L14mC41 <94200536+L14mC41@users.noreply.github.com>
2022-04-03 08:32:19 -0700

    month syntax check
    Wrote preliminary syntax checker
    Only allows for regular month input
    Can be a test before running converter/s

--- scripts.js
+++ scripts.js
@@ -1,1 +1,27 @@
+// While If Month to Days Converter
 
+// Prompt User to Choose and enter any Month
+var monthEntry = prompt("Hello, Please enter any Month:");
+
+// Default Value
+check = false
+while (check==false) {
+
+    /* Check if user input is in fridge - Repeat prompt until user guesses valid fruit - could use an array here for listing correct guesses */
+
+    var check = (monthEntry == "January") || (monthEntry == "February") || (monthEntry == "March") || (monthEntry == "April") || (monthEntry == "May") || (monthEntry == "June") || (monthEntry == "July") || (monthEntry == "August") || (monthEntry == "September") || (monthEntry == "October") || (monthEntry == "November") || (monthEntry == "December") || (monthEntry == "january") || (monthEntry == "february") || (monthEntry == "march") || (monthEntry == "april") || (monthEntry == "may") || (monthEntry == "june") || (monthEntry == "july") || (monthEntry == "august") || (monthEntry == "september") || (monthEntry == "october") || (monthEntry == "november") || (monthEntry == "december");
+
+    // If Month Entry is Accepted from above list
+    if (check) {
+      console.log(`${monthEntry} is a month!`);
+      /* document.getElementById("test2").className = "test"; */
+      document.getElementById("test").style.backgroundColor = "purple";
+      check = true;
+    }
+
+    // Month Entry is not Accepted
+    else {
+      console.log(`${monthEntry} is NOT an accepted syntax for month.`);
+      monthEntry = prompt("Month not accepted in this format. Please try again");
+    }
+}
